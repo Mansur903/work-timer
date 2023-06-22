ea9c9af4a6673306750cf3f572e745a3cbe9c5bf
Mansur <mansur.khusnutdinov@underhood.dev>
2023-06-22 22:11:52 +0300

    initializing server 1.1

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
 const PORT = process.env.PORT || 6000
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.status(200).json({message: 'WORKING!!!'})
+})
 
 const start = async () => {
 	try {
--- server/models/models.js
+++ server/models/models.js
@@ -6,3 +6,7 @@ const User = sequelize.define('user', {
 	email: {type: DataTypes.STRING, unique: true},
 	password: {type: DataTypes.STRING}
 })
+
+module.exports = {
+	User
+}
