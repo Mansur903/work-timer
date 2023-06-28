4fa3ddda1e9b08718e0db9739ee42c99ab72a82c
Mansur <mansur.khusnutdinov@underhood.dev>
2023-06-29 00:13:58 +0300

    added user entity and token (need corrections)

--- client/src/entities/user/model.ts
+++ client/src/entities/user/model.ts
@@ -2,3 +2,57 @@ import {createSelector, createSlice, Dispatch, PayloadAction} from '@reduxjs/too
 import axios, {AxiosResponse} from 'axios'
 // import {useQuery} from 'react-query'
 import {useSelector} from 'react-redux'
+import {deleteToken, setToken} from 'shared/lib'
+
+export type User = {
+  id: number
+  email: string
+}
+
+type InitialStateProps = User & {
+  isAuthenticated: boolean,
+}
+
+const initialState: InitialStateProps = {
+  id: 0,
+  email: '',
+  isAuthenticated: false
+}
+
+export const userModel = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, {payload}: PayloadAction<User | InitialStateProps>) =>
+      ({...state, ...payload, isAuthenticated: true}),
+    clearUser: () => {
+      deleteToken()
+      return initialState
+    }
+  }
+})
+
+export const {setUser, clearUser} = userModel.actions
+
+export const UserAuth = (login: string, password: string, dispatch: Dispatch) =>
+  useQuery<AxiosResponse<{token: string, userData: User}>, ErrorResponsesType>(
+    'userAuth',
+    () => axios.post('/user/login', {login, password}),
+    {
+      onSuccess: ({data}) => {
+        setToken(data.token)
+        setUserLs(data.userData)
+        dispatch(setUser(data.userData))
+      },
+      enabled: false,
+      refetchOnWindowFocus: false,
+      retry: false,
+    }
+  )
+
+export const useIsAuthenticated = () => useSelector(
+  createSelector(
+    (state: RootState) => state.user.isAuthenticated,
+    (isAuthenticated) => isAuthenticated
+  )
+)
---
+++ client/src/shared/lib/token.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export const TOKEN_KEY = 'auth'
+
+export const setBearer = (token: string) => {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+export const setToken = (token: string) => {
+  localStorage.setItem(TOKEN_KEY, JSON.stringify({token}))
+  setBearer(token)
+}
+
+export const deleteToken = () => localStorage.removeItem(TOKEN_KEY)
+
+export const getToken = (): string | null => {
+  const token = localStorage.getItem(TOKEN_KEY)
+  if (!token) return null
+
+  try {
+    return JSON.parse(token)
+  } catch {
+    deleteToken()
+  }
+
+  return null
+}
