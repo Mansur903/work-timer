147e038506d5b8e5eb8a0d32175563106894d9b3
Mansur <mansur.khusnutdinov@underhood.dev>
2023-09-16 00:12:55 +0300

    using directives with inputs, trying create login form

---
+++ client/src/components/LoginPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+const header = ref('Login Page')
+const login = ref('')
+const password = ref('')
+
+const handleLogin = () => {
+  console.log('logged in!')
+}
+</script>
+
+<template>
+  <form class="container" @:submit.prevent="handleLogin">
+    <h1>{{ header }}</h1>
+    <input v-model.trim="login" class="login" type="text" placeholder="Login">
+    <input v-model="password" class="password" type="password" placeholder="Password">
+    <button @:click="handleLogin">Login</button>
+    {{login}}
+  </form>
+
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ client/src/views/LoginPageView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import LoginPage from '../components/LoginPage.vue'
+
+</script>
+
+<template>
+  <div class="login">
+    <LoginPage />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .login {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
